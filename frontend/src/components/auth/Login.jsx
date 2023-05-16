4664d9d770e1e6a239e42dd8b820a5710f5a748b
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-16 19:59:51 +0400

    created login page,and added validations.

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom';
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const {  email, password,  } = formData;
+  //for more useage of code
+  const onChange = e => setFormData({...formData,[e.target.name]:e.target.value})
+
+  //onSubimit to store input data in it
+
+  const onSubmit = async e => {
+    e.preventDefault();
+    
+    console.log('succes');
+    
+    
+  }
   return (
-    <div>Login</div>
+    <section className="container">
+      <div className="alert alert-danger">
+        Invalid credentials
+      </div>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+      <form className="form" action="dashboard.html">
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </section>
   )
 }
 
