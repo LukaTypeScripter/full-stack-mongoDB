3d1d02f8d5b88b69ee1b33b4000a7b7da8e0fb71
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-18 22:06:03 +0400

    bug to fix.

--- frontend/src/actions/auth.js
+++ frontend/src/actions/auth.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { REGISTER_SUCCESS,REGISTER_FAIL,USER_LOADED,AUTH_ERROR } from './type';
+import { REGISTER_SUCCESS,REGISTER_FAIL,USER_LOADED,AUTH_ERROR,LOGIN_SUCCESS,LOGIN_FAIL } from './type';
 import { setAlert } from './alert';
 import setAuthToken from '../utils/setAuthyToken';
 //load user
@@ -41,12 +41,13 @@ export const register = ({name, email, password}) => async dispatch => {
     const body = JSON.stringify({name, email, password});
 
     try {
-        const res = await axios.post('/api/users',body,config);
+        const res = await axios.post('http://localhost:5000/api/users',body,config);
         
         dispatch({
             type: REGISTER_SUCCESS,
             payload: res.data
         })
+        dispatch(loadUser());
     } catch (err) {
         const error = err.response.data.errors;
 
@@ -58,6 +59,39 @@ export const register = ({name, email, password}) => async dispatch => {
         console.log(err);
         dispatch({
             type: REGISTER_FAIL
+        })
+    }
+}
+
+//login user
+export const loginUser = ({ email, password}) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const body = JSON.stringify({ email, password});
+
+    try {
+        const res = await axios.post('http://localhost:5000/api/auth',body,config);
+        
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        })
+        dispatch(loadUser());
+    } catch (err) {
+        const error = err.response.data.errors;
+
+        if(error){
+            error.forEach(err => dispatch(setAlert(err.msg, 'danger')));
+        }
+
+
+        console.log(err);
+        dispatch({
+            type: LOGIN_FAIL
         })
     }
 }
