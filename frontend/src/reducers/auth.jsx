e89e059a1c9f6982937c3ff6dc9f4be7d3946cb7
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-18 22:06:44 +0400

    added login logic.

--- frontend/src/reducers/auth.jsx
+++ frontend/src/reducers/auth.jsx
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS,REGISTER_FAIL,USER_LOADED,AUTH_ERROR } from "../actions/type";
+import { REGISTER_SUCCESS,REGISTER_FAIL,USER_LOADED,AUTH_ERROR,LOGIN_FAIL,LOGIN_SUCCESS } from "../actions/type";
 const initialState = {
     user: null,
     token: localStorage.getItem("token"),
@@ -17,6 +17,7 @@ export default function (state = initialState, action) {
                 user: payload
             }
         case REGISTER_SUCCESS:
+        case LOGIN_SUCCESS:
             localStorage.setItem("token",payload.token);
             return {
               ...state,
@@ -27,6 +28,7 @@ export default function (state = initialState, action) {
             }
         case REGISTER_FAIL:
         case AUTH_ERROR:
+        case LOGIN_FAIL:
             localStorage.removeItem("token");
             return {
              ...state,
