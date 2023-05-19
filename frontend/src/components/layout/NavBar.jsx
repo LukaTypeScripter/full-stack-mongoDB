bf97a5ea5141af447a7b718f17f79a848034116c
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-19 15:36:16 +0400

    added dinamic func

--- frontend/src/components/layout/NavBar.jsx
+++ frontend/src/components/layout/NavBar.jsx
@@ -1,17 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {logout} from '../../actions/auth'
+ function NavBar({auth: {isAuthenticated,loading},logout}) {
+  const authLinks = (
+    <ul>
+    <li>
+      <Link onClick={logout} href="#">
+        <i className="fas fa-sign-out-alt"></i>{' '}
+      <span className="hide-sm">Logout</span> </Link>
+    </li>
+   
+  </ul>
+  )
 
-export default function NavBar() {
+  const guestLinks = (
+    <ul>
+        <li>
+          <Link href="#!">Developers</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+  )
+
+  
+  
+  
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <Link to={"/"}><i className="fas fa-code"></i> DevConnector</Link>
+        <Link to={"/"}>
+          <i className="fas fa-code"></i> DevConnector
+        </Link>
       </h1>
-      <ul>
-        <li><Link href="#">Developers</Link></li>
-        <li><Link to="/register">Register</Link></li>
-        <li><Link to="/login">Login</Link></li>
-      </ul>
+      {!loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
     </nav>
-  )
+  );
 }
+
+NavBar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+  auth: state.auth
+})
+export default connect(mapStateToProps,{logout})(NavBar);
