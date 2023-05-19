cce1fa10fd24a55cb2a23cdebb05a8962463fc56
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-19 15:36:46 +0400

    added naviagtion.

--- frontend/src/components/auth/Register.jsx
+++ frontend/src/components/auth/Register.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import axios from 'axios'
-import { Link } from 'react-router-dom';
+import { Link,Navigate } from 'react-router-dom';
 import { connect } from "react-redux";
 import { setAlert } from "../../actions/alert";
 import { register } from "../../actions/auth";
 import PropTypes from 'prop-types'
-function Register({setAlert,register}) {
+function Register({setAlert,register,isAuthenticated}) {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -26,6 +26,10 @@ function Register({setAlert,register}) {
     register({name, email, password});
     
     }
+  }
+
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
   }
 
   return (
@@ -85,7 +89,11 @@ function Register({setAlert,register}) {
 }
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
-  register: PropTypes.func.isRequired
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
 };
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
 
-export default connect(null,{setAlert,register})(Register);
+export default connect(mapStateToProps,{setAlert,register})(Register);
