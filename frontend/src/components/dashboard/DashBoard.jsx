99b35e9137315d70f34644953f487bd139068752
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-20 14:48:21 +0400

    made some thing dinamic.

--- frontend/src/components/dashboard/DashBoard.jsx
+++ frontend/src/components/dashboard/DashBoard.jsx
@@ -1,100 +1,108 @@
-import React from 'react'
+import React,{Fragment, useEffect} from 'react'
 import PropTypes from 'prop-types'
-
-const DashBoard = props => {
-  return (
+import { Link, Outlet } from 'react-router-dom'
+import {connect} from 'react-redux'
+import { getCurrentProfile } from '../../actions/profile'
+import Spinner from '../layout/spinner'
+const DashBoard = ({getCurrentProfile,auth:{user},profile:{profile,loading}}) => {
+  useEffect(() => {
+    getCurrentProfile();
+    }, [])
+  return loading && profile === null ? <Spinner/> : <Fragment>
     <section className="container">
-    <h1 className="large text-primary">
-      Dashboard
-    </h1>
-    <p className="lead"><i className="fas fa-user"></i> Welcome John Doe</p>
-    <div className="dash-buttons">
-      <a href="edit-profile.html" className="btn btn-light"
-        ><i className="fas fa-user-circle text-primary"></i> Edit Profile</a
-      >
-      <a href="add-experience.html" className="btn btn-light"
-        ><i className="fab fa-black-tie text-primary"></i> Add Experience</a
-      >
-      <a href="add-education.html" className="btn btn-light"
-        ><i className="fas fa-graduation-cap text-primary"></i> Add Education</a
-      >
-    </div>
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome {user && user.name}
+      </p>
+      {profile !== null ? <Fragment>has</Fragment> : <Fragment>you have not yet setup profile,please add some more info.
+        <Link to="/create-profile" className='btn btn-primary my-1'>Create Profile</Link>
+        </Fragment>}
+      <div className="dash-buttons">
+        <a href="edit-profile.html" className="btn btn-light">
+          <i className="fas fa-user-circle text-primary"></i> Edit Profile
+        </a>
+        <a href="add-experience.html" className="btn btn-light">
+          <i className="fab fa-black-tie text-primary"></i> Add Experience
+        </a>
+        <a href="add-education.html" className="btn btn-light">
+          <i className="fas fa-graduation-cap text-primary"></i> Add Education
+        </a>
+      </div>
 
-    <h2 className="my-2">Experience Credentials</h2>
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Company</th>
-          <th className="hide-sm">Title</th>
-          <th className="hide-sm">Years</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Tech Guy Web Solutions</td>
-          <td className="hide-sm">Senior Developer</td>
-          <td className="hide-sm">
-            02-03-2009 - 01-02-2014
-          </td>
-          <td>
-            <button className="btn btn-danger">
-              Delete
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <td>Traversy Media</td>
-          <td className="hide-sm">Instructor & Developer</td>
-          <td className="hide-sm">
-            02-03-2015 - Now
-          </td>
-          <td>
-            <button className="btn btn-danger">
-              Delete
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+      <h2 className="my-2">Experience Credentials</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Company</th>
+            <th className="hide-sm">Title</th>
+            <th className="hide-sm">Years</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Tech Guy Web Solutions</td>
+            <td className="hide-sm">Senior Developer</td>
+            <td className="hide-sm">02-03-2009 - 01-02-2014</td>
+            <td>
+              <button className="btn btn-danger">Delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Traversy Media</td>
+            <td className="hide-sm">Instructor & Developer</td>
+            <td className="hide-sm">02-03-2015 - Now</td>
+            <td>
+              <button className="btn btn-danger">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
 
-    <h2 className="my-2">Education Credentials</h2>
-    <table className="table">
+      <h2 className="my-2">Education Credentials</h2>
+      <table className="table">
         <thead>
           <tr>
             <th>School</th>
             <th className="hide-sm">Degree</th>
             <th className="hide-sm">Years</th>
-            <th />
+            <th></th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <td>Northern Essex</td>
             <td className="hide-sm">Associates</td>
-            <td className="hide-sm">
-              02-03-2007 - 01-02-2009
-            </td>
+            <td className="hide-sm">02-03-2007 - 01-02-2009</td>
             <td>
-              <button className="btn btn-danger">
-                Delete
-              </button>
+              <button className="btn btn-danger">Delete</button>
             </td>
           </tr>
         </tbody>
       </table>
 
       <div className="my-2">
-          <button className="btn btn-danger">
-              <i className="fas fa-user-minus"></i>
-
-              Delete My Account
-          </button>
-        </div>
-  </section>
-  )
+        <button className="btn btn-danger">
+          <i className="fas fa-user-minus"></i>
+          Delete My Account
+        </button>
+      </div>
+      <Outlet />
+    </section>
+  </Fragment>
+   
+  
 }
 
-DashBoard.propTypes = {}
+DashBoard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
+}
 
-export default DashBoard
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+})
+
+export default connect(mapStateToProps,{getCurrentProfile})(DashBoard) 
