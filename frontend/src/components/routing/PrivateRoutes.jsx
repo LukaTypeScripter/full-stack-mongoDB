90e6f32fcaafe88a051e68c53137cc69a13db218
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-20 14:46:19 +0400

    fix bug.

--- frontend/src/components/routing/PrivateRoutes.jsx
+++ frontend/src/components/routing/PrivateRoutes.jsx
@@ -1,22 +1,11 @@
 import React from "react";
-import { Route, Navigate } from "react-router-dom";
+import { Route, Navigate,Outlet } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 const PrivateRoutes = ({
-  component: Component,
   auth: { isAuthenticated, loading },
-  ...rest
 }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      !isAuthenticated && !loading ? (
-        <Navigate to="/login" />
-      ) : (
-        <Component {...props} />
-      )
-    }
-  />
+  !isAuthenticated && !loading ? <Navigate to='/login'/> :  <Outlet/>
 );
 
 PrivateRoutes.propTypes = {
