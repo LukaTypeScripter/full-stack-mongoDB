cb1544f68cad47f92648765c003bbea468f511ef
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-20 14:48:30 +0400

    added new types.

--- frontend/src/actions/type.js
+++ frontend/src/actions/type.js
@@ -7,4 +7,10 @@ export const AUTH_ERROR = 'AUTH_ERROR';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
 export const LOGOUT = 'LOGOUT';
+export const GET_PROFILE = 'GET_PROFILE';
+export const GET_PROFILES = 'GET_PROFILES';
+export const GET_REPOS = 'GET_REPOS';
+export const PROFILE_ERROR = 'GET_PROFILE';
+export const CLEAR_PROFILE = 'GET_PROFILES';
+
 
