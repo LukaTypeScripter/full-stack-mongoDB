c5d547befdae220a2ec2f709a0fd369d9920a508
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-24 19:02:01 +0400

    added new function.

--- frontend/src/actions/profile.js
+++ frontend/src/actions/profile.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import {GET_PROFILE,GET_PROFILES,GET_REPOS,PROFILE_ERROR,CLEAR_PROFILE} from './type'
+import {GET_PROFILE,GET_PROFILES,GET_REPOS,PROFILE_ERROR,CLEAR_PROFILE,UPDATE_PROFILE} from './type'
 import { Navigate } from 'react-router-dom';
 //get current users prof
 
@@ -39,6 +39,74 @@ export const createProfile = (formData, navigate, edit = false) => async dispatc
   if(!edit) {
     navigate('/dashboard');
   }
+
+} catch (err) {
+    const error = err.response.data.errors;
+
+        if(error){
+            error.forEach(err => dispatch(setAlert(err.msg, 'danger')));
+        }
+    dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+  }
+}
+
+
+//adding expp
+
+
+export const addExperience = (formData, navigate) => async dispatch => {
+  try {
+    const config = {
+        headers: {
+          'Content-Type':'application/json'
+        }
+    }
+    const res = await axios.put('http://localhost:5000/api/profile/experience', formData, config);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert( 'experience added','success'));
+    navigate('/dashboard');
+  
+
+} catch (err) {
+    const error = err.response.data.errors;
+
+        if(error){
+            error.forEach(err => dispatch(setAlert(err.msg, 'danger')));
+        }
+    dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+  }
+}
+
+
+//adding education
+
+
+export const addEducation = (formData, navigate) => async dispatch => {
+  try {
+    const config = {
+        headers: {
+          'Content-Type':'application/json'
+        }
+    }
+    const res = await axios.put('http://localhost:5000/api/profile/education', formData, config);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert( 'education added','success'));
+    navigate('/dashboard');
+  
 
 } catch (err) {
     const error = err.response.data.errors;
