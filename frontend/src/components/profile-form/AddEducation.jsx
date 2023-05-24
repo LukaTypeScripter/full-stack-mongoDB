a6e44f1a744cb2a86a9a5c22b87cafd386c0fe94
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-24 19:02:21 +0400

    education

---
+++ frontend/src/components/profile-form/AddEducation.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import PropTypes from 'prop-types'
+// import {connect} from 'react-redux';
+// import {Link,useNavigate} from'react-router-dom'
+// import addExperience from '../../actions/profile'
+// const AddEducation = props => {
+//     const navigate = useNavigate()
+//   return (
+//     <div>AddEducation</div>
+//   )
+// }
+
+// AddEducation.propTypes = {
+//     addExperience: PropTypes.func.isRequired
+// }
+
+// export default connect(null,{addExperience})(AddEducation) 
