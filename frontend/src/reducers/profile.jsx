47ecbfeeed5505e0df1964454f36bc7e9b0511e2
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-24 19:02:46 +0400

    profile update reducer

--- frontend/src/reducers/profile.jsx
+++ frontend/src/reducers/profile.jsx
@@ -1,38 +1,45 @@
-
-import {GET_PROFILE,GET_PROFILES,GET_REPOS,PROFILE_ERROR,CLEAR_PROFILE} from '../actions/type'
+import {
+  GET_PROFILE,
+  GET_PROFILES,
+  GET_REPOS,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE
+} from "../actions/type";
 
 const initialState = {
-    profile:null,
-    profiles:[],
-    repos:[],
-    loading:true,
-    error:{}
-}
+  profile: null,
+  profiles: [],
+  repos: [],
+  loading: true,
+  error: {},
+};
 
+export default function (state = initialState, action) {
+  const { type, payload } = action;
 
-export default function(state=initialState,action) {
-    const {type,payload} = action;
-
-    switch(type) {
-            case GET_PROFILE:
-                return{
-                    ...state,
-                    profile:payload,
-                    loading:false
-                }
-            case PROFILE_ERROR:
-                    return {
-                        profile:null,
-                        loading:false,
-                        error:payload
-                    }
-            case CLEAR_PROFILE:
-                return {
-                ...state,
-                profile:null,
-                repos:[],
-                loading:false
-                }
-            default: return state;
-        }
+  switch (type) {
+    case GET_PROFILE:
+ case UPDATE_PROFILE:
+      return {
+        ...state,
+        profile: payload,
+        loading: false,
+      };
+    case PROFILE_ERROR:
+      return {
+        profile: null,
+        loading: false,
+        error: payload,
+      };
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
+        loading: false,
+      };
+    default:
+      return state;
+  }
 }
